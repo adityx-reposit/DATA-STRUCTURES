7cb6fc0ca1bc00047e227c4b6304474ea37f251e
adityx-reposit <adityayadav2022@vitbhopal.ac.in>
2024-08-03 12:39:04 +0530

    search added

--- 2D ARRAY/create.java
+++ 2D ARRAY/create.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 
 public class create {
+     public static boolean search(int matrix[][], int key) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){  
+                 if(matrix[i][j]==key){
+                 System.out.println("element found at cell "+ i +","+ j);
+                 
+                    return true;
+               }
+            }
+            
+        }
+        System.out.println("not found");
+        return false;
+    }
+
+
+
  public static void main(String[] args) {
     int matrix[][]= new int[3][3];
     int n=matrix.length,m=matrix[0].length;
@@ -15,9 +32,12 @@ public class create {
                 for(int j=0;j<m;j++){
                     System.out.print(matrix[i][j] +" ");
                 }
-                    System.out.println();
+                    System.out.println();        
+            }
+                search(matrix, 4);
+ 
+            }     
 
-        
+
 }
- }
-}
+
